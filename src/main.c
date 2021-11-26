8ce8f17c6920ed763ff07811ef31d4f1d737a588
redquartista <redquartista@gmail.com>
2021-11-26 23:34:31 +0100

    Added the TS and sample file code

--- src/main.c
+++ src/main.c
@@ -7,3 +7,35 @@ int main(void) {
 	return 0;
 
 }
+
+/*
+#include <stdio.h>
+
+int main(void){
+
+    FILE *ts, *ts_out;
+    ts = fopen("/home/pi/mpeg_ts_assignment/watermarking_test_task.ts", "rb");
+    ts_out =  fopen("output.txt", "w+");
+    if(ts!=NULL){
+        printf("\n\nSOF\n\n");
+        char ch = 0;
+        while (ch != EOF){
+
+            ch = fgetc(ts);
+	    if(ch==0x47)
+	    {
+            	fprintf(ts_out, "\n%x", ch);
+	    }
+	    else
+	    {
+	        fprintf(ts_out, "%x", ch);
+	    }
+	    
+        }
+        printf("\n\nEOF");
+    }
+    fclose(ts);
+    return 0;
+}
+
+*/
