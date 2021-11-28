98ecababdc94b1f33a85b95683d936c9995d761c
redquartista <redquartista@gmail.com>
2021-11-28 22:11:58 +0100

    array of packets is not needed

--- includes/ts.c
+++ includes/ts.c
@@ -6,7 +6,7 @@
 #define TS_PACKET_PAYLOAD_BYTES 184
 
 static int readTSPacket(FILE *ts_file, tsPacket_t *ts_packet) {
-    fread(ts_packet, 1, TS_PACKET_BYTES, ts_file);
+    fread(ts_packet, TS_PACKET_BYTES, 1, ts_file);
     if(ts_packet->header.syncbyte != 0x47)  {
         printf("\n\nERROR: Packet out of sync. Sync byte != 0x47\n\n");
         return 0;
@@ -20,14 +20,13 @@ static int readTSPacket(FILE *ts_file, tsPacket_t *ts_packet) {
 };
 
 static unsigned long fileSize(FILE *ts_file){
-
     fseek(ts_file, 0, SEEK_END);
     unsigned long fsize = ftell(ts_file);
     fseek(ts_file, 0, SEEK_SET);
     return fsize;
 }
-static void printTSPacket(tsPacket_t ts) {
 
+static void printTSPacket(tsPacket_t ts) {
     printf("syncbyte: %02x\n", (unsigned int) ts.header.syncbyte);
     printf("tei: %01x\n", (unsigned int) ts.header.tei);
     printf("pusi: %01x\n", (unsigned int) ts.header.pusi);
@@ -59,33 +58,33 @@ int tsDemux(char *argv[]) {
 
         printf("File size = %u bytes, file has %u TS packets\n\n", (unsigned int)fsize, (unsigned int) fsizePackets);
         
-        tsPacket_t *ts_packet = (tsPacket_t *) malloc((fsizePackets * sizeof(tsPacket_t)) + 1);
-        memset((void *)ts_packet, 0, (fsizePackets * sizeof(tsPacket_t)));
+        tsPacket_t ts_packet;// = (tsPacket_t *) malloc((fsizePackets * sizeof(tsPacket_t)) + 1);
+        memset((void *)&ts_packet, 0, sizeof(tsPacket_t));
 
         // fread(ts_packet, TS_PACKET_BYTES, fsizePackets, ts_file);
 
         uint32_t packetcnt = 0;
         while(packetcnt<fsizePackets) {
-            if(!readTSPacket(ts_file, &ts_packet[packetcnt])) {
-                free(ts_packet);
+            if(!readTSPacket(ts_file, &ts_packet)) {
+                //free(ts_packet);
                 fclose(ts_file);
                 return 0;
             }
             printf("\n\npacket #%u\n", packetcnt);
-            printTSPacket(ts_packet[packetcnt]);
+            printTSPacket(ts_packet);
             packetcnt++;
         } 
 
         int eof = getc(ts_file);
         if( eof != EOF) {
             printf("\n\nERROR: EOF could not be detected\n\n");
-            free(ts_packet);
+            //free(ts_packet);
             fclose(ts_file);
             return 0;
         }
         else {
             printf("\n\nEnd of File\n\n");
-            free(ts_packet);
+            //free(ts_packet);
         }
     }
     else {
