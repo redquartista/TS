89cfaf7c5c2ea7d9052846f812a2ded8eed518b1
hrudaynath@X1Carbon <redquartista@gmail.com>
2022-06-24 23:45:59 +0200

    Added comments, return codes and doxygen descriptions

--- includes/ts.c
+++ includes/ts.c
@@ -2,33 +2,34 @@
 #include <stdlib.h>
 #include "ts.h"
 
-#define TS_PACKET_BYTES 188
-#define TS_PACKET_PAYLOAD_BYTES 184
-
-/*Temporarily hardcoding the values (possibly incorrect) for demo*/
-#define VIDEO_PID 0x428
-#define AUDIO_PID 0x448
-
-
 static uint32_t getAudioPID() {
+
     /*TODO: Find how to get audio PID*/
-    return AUDIO_PID;
+    uint32_t pid = AUDIO_PID;
+    return pid;
 }
 
 static uint32_t getVideoPID() {
+
     /*TODO: Find how to get audio PID*/
-    return VIDEO_PID;
+    uint32_t pid = VIDEO_PID;
+
+    return pid;
 }
 
-static int proecssTSPacket(FILE *ts_file, tsPacket_t *ts_packet, char *video_loc, char* audio_loc, uint32_t videoPID, uint32_t audioPID) {
+static status_e proecssTSPacket(FILE *ts_file, tsPacket_t *ts_packet, char *video_loc, char* audio_loc, uint32_t videoPID, uint32_t audioPID) {
+    
     fread(ts_packet, TS_PACKET_BYTES, 1, ts_file);
+    
+    //Return error of syncbyte is not found
     if(ts_packet->header.syncbyte != 0x47)  {
         printf("\n\nERROR: Packet out of sync. Sync byte != 0x47\n\n");
-        return 0;
+        return error_e;
     }
 
     FILE *f_pid;
 
+    //identify the type of packet based on the PID and open the respective output file in append mode
     if(ts_packet->header.pid == audioPID){
         f_pid = fopen(audio_loc, "a");
     }
@@ -38,12 +39,14 @@ static int proecssTSPacket(FILE *ts_file, tsPacket_t *ts_packet, char *video_loc
     else
     {
         //TODO: Identify and read tables.
-        return 1;
+        return success_e;
     }
+
     //TODO: Identify and remove adaptation field
+    //write the payload to the appropriate file
     fwrite(ts_packet->payload, TS_PACKET_PAYLOAD_BYTES, 1, f_pid);
     fclose(f_pid);
-    return 1;
+    return success_e;
 };
 
 static unsigned long fileSize(FILE *ts_file){
@@ -54,6 +57,7 @@ static unsigned long fileSize(FILE *ts_file){
 }
 
 #ifdef PRINT_PACKETS
+
 static void printTSPacket(tsPacket_t ts) {
     printf("syncbyte: %02x\n", (unsigned int) ts.header.syncbyte);
     printf("tei: %01x\n", (unsigned int) ts.header.tei);
@@ -64,18 +68,20 @@ static void printTSPacket(tsPacket_t ts) {
     printf("afc: %01x\n", (unsigned int) ts.header.afc);
     printf("cc: %01x\n", (unsigned int) ts.header.cc);
     
-    // int i = 0;
-    // printf("Payload: ");
-    // for(i=0; i<184; i++)
-    // printf("02%x", (unsigned int) ts.payload[i]);
+    printf("Payload: ");
+
+    for(int i=0; i<184; i++)
+        printf("02%x", (unsigned int) ts.payload[i]);
 
     printf("\n");
 }
 #endif
 
-int tsDemux(char *argv[]) {
+status_e tsDemux(char *argv[]) {
 
     char *ts_loc = argv[1], *video_loc = argv[2], *audio_loc = argv[3];
+    
+    //Open the transport stream file in binary read mode
     FILE *ts_file = fopen(ts_loc, "rb");
     
     if(ts_file != NULL) {
@@ -86,36 +92,37 @@ int tsDemux(char *argv[]) {
         loc_file  = fopen(video_loc, "w");
         if(loc_file == NULL) {
             printf("Error opening %s", video_loc);
-            return 0;
+            return error_e;
         }
         fclose(loc_file);
 
         loc_file  = fopen(audio_loc, "w");
         if(loc_file == NULL) {
             printf("Error opening %s", audio_loc);
-            return 0;
+            return error_e;
         }
+
         fclose(loc_file);
 
+        //Calculate how many TS packets are contained in the .ts file
         uint32_t fsize = fileSize(ts_file);
         uint32_t fsizePackets = fsize / TS_PACKET_BYTES;
 
         printf("File size = %u bytes, file has %u TS packets\n\n", (unsigned int)fsize, (unsigned int) fsizePackets);
         
+        //Declare and initialze a .ts packet to 0
         tsPacket_t ts_packet;
         memset((void *)&ts_packet, 0, sizeof(tsPacket_t));
 
-        //tsPacket_t *ts_packet = (tsPacket_t *) malloc((fsizePackets * sizeof(tsPacket_t)) + 1);
-        //fread(ts_packet, TS_PACKET_BYTES, fsizePackets, ts_file);
         uint32_t packetcnt = 0;
         uint32_t audioPID = getAudioPID();
         uint32_t videoPID = getVideoPID();
         
+        //Process packet by packet and group into audio and video packet
         while(packetcnt<fsizePackets) {
             if(!proecssTSPacket(ts_file, &ts_packet, video_loc, audio_loc, videoPID, audioPID)) {
-                //free(ts_packet);
                 fclose(ts_file);
-                return 0;
+                return error_e;
             }
 
 #ifdef PRINT_PACKETS
@@ -128,21 +135,19 @@ int tsDemux(char *argv[]) {
         int eof = getc(ts_file);
         if( eof != EOF) {
             printf("\n\nERROR: EOF could not be detected\n\n");
-            //free(ts_packet);
             fclose(ts_file);
-            return 0;
+            return error_e;
         }
         else {
             printf("\n\nEnd of File\n\n");
-            //free(ts_packet);
         }
     }
     else {
         printf("\n\nFile %s does not exist\n\n", ts_loc);
         fclose(ts_file);
-        return 0;
+        return error_e;
     }
 
     fclose(ts_file);
-    return 1;
+    return success_e;
 }
--- includes/ts.h
+++ includes/ts.h
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define TS_PACKET_BYTES                 (uint32_t) 188ul
+#define TS_PACKET_PAYLOAD_BYTES         (uint32_t) 184ul
 
+#define VIDEO_PID                       (uint32_t) 0x424ul
+#define AUDIO_PID                       (uint32_t) 0x428ul
+
+/**
+ * @brief An enum to codify error and success states
+ * 
+ */
+typedef enum {
+    error_e = 0,
+    success_e
+} status_e;
+
+/**
+ * @brief A structure to contain the header of a transport packet
+ * 
+ */
 typedef union {
     uint32_t value;
     struct {
@@ -16,9 +34,19 @@ typedef union {
     };
 } tsHeader_t;
 
+/**
+ * @brief A structure to capture transport stream packet
+ * 
+ */
 typedef struct {
     tsHeader_t header;
     uint8_t payload[184]; //TODO: Support adaptation field
 } tsPacket_t;
 
-int tsDemux(char *argv[]);
+/**
+ * @brief The function demultiplexes a transport stream and generates separate audio and video file
+ * 
+ * @param argv arguments passed on command line to indicate <path to input .ts file> <path to output .mp4 file> <path to .wav file>
+ * @return status_e return success or error
+ */
+status_e tsDemux(char *argv[]);
